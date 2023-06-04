3cb1160f2c8ee87ca8aee14f22b17f0f055b5990
Sinovuyo Ngolomi <sinovuyo@developmenthub.co.za>
2023-06-04 11:19:36 +0200

    Added functionality

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "6c35b4d01ee348e4afb4923aee77de87"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
